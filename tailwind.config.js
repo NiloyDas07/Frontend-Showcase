7069a82136532c183c8f3c2aacb65f91ceb8a8ec
NSD <niloysd7@gmail.com>
2025-05-31 15:02:23 +0530

    update tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{astro,html,js,jsx,ts,tsx}",   // All your templates & components
+      "./src/styles/**/*.css"                    // Any CSS that uses @apply or references Tailwind classes
+    ],
+  };
+  

13e112d681493f6166f13123ff23caad26cae9af
NSD <niloysd7@gmail.com>
2025-05-31 15:05:30 +0530

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      "./src/**/*.{astro,html,js,jsx,ts,tsx}",   // All your templates & components
-      "./src/styles/**/*.css"                    // Any CSS that uses @apply or references Tailwind classes
-    ],
-  };
-  
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // All your templates & components
+    "./src/styles/**/*.css", // Any CSS that uses @apply or references Tailwind classes
+  ],
+};
